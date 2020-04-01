e50e606e8372a9295427a5f7d2702e6979537fc8
Letícia Lima <lelima039@gmail.com>
2020-04-01 12:32:37 -0300

    Add files via upload

---
+++ genoma.py
@@ -0,0 +1,29 @@
+entrada = open("human.fasta").read()
+saida = open("hum.html", "w")
+
+contador = {}
+
+for i in ['A', 'T', 'C', 'G']:
+    for j in ['A', 'T', 'C', 'G']:
+        contador[i+j] = 0
+
+entrada = entrada.replace('\n', '')
+
+for k in range(len(entrada)-1):
+    contador[entrada[k]+entrada[k+1]] += 1
+
+# HTML
+
+saida.write("<div>")
+
+i = 1
+for k in contador:
+    transparencia = contador[k]/max(contador.values())
+    saida.write("<div style='width:100px; border:1px solid #111; color:#fff; height:100px; float:left; "
+                "background-color:rgba(0, 0, 0, " + str(transparencia) + "')>" + k + "</div>")
+    if i % 4 == 0:
+        saida.write("<div style='clear:both'></div>")
+
+    i += 1
+
+saida.close()
